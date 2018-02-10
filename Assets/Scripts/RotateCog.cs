8dcc9a1611f1a7f7544668b9001a0ca6bf2596f5
Robert D <robert.desrosiers@teamgiga.com>
2018-02-10 11:30:41 -0500

    v0.2.0 - RotateCog Script Added
    Added RotateCog script. Can customize the degrees of rotation per second.

---
+++ Assets/Scripts/RotateCog.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCog : MonoBehaviour {
+
+    public float degreesPerSecond;
+
+	void Update () {
+        transform.Rotate(Vector3.up, degreesPerSecond * Time.deltaTime, Space.Self);
+	}
+}
