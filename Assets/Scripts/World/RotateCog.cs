1d8d2787f16cafe7d8426a6504b8c87533ff3132
Robert D <robert.desrosiers@teamgiga.com>
2018-02-24 00:35:47 -0500

    v0.5.3 - Test Scene
    Created test scene for flying around moving obstacles. Also improved on RotateCog script so rotation on all axes can be edited.

--- Assets/Scripts/World/RotateCog.cs
+++ Assets/Scripts/World/RotateCog.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class RotateCog : MonoBehaviour {
 
-    public float degreesPerSecond;
+    public float xDegreesPerSecond;
+    public float yDegreesPerSecond;
+    public float zDegreesPerSecond;
 
-	void Update () {
-        transform.Rotate(Vector3.up, degreesPerSecond * Time.deltaTime, Space.Self);
-	}
+
+    public bool rotateX;
+    public bool rotateY;
+    public bool rotateZ;
+
+	void FixedUpdate () {
+
+        if (rotateX)
+            transform.Rotate(Vector3.right, xDegreesPerSecond * Time.deltaTime, Space.Self);
+
+        if (rotateY)
+            transform.Rotate(Vector3.up, yDegreesPerSecond * Time.deltaTime, Space.Self);
+
+        if (rotateY)
+            transform.Rotate(Vector3.forward, zDegreesPerSecond * Time.deltaTime, Space.Self);
+    }
 }
